7f01aa4e04db8706c0f76460cf7413ce04891301
Andre Roman Quispe <andre.romanzzz@gmail.com>
2026-09-22 22:50:09 -0500

    Implementacion de Pregunta 7 por Andre Roman

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,16 @@ plugins {
 
 android {
     namespace = "pe.edu.cibertec.appgrupo1"
-    compileSdk {
-        version = release(36)
+    compileSdk = 35
+
+    buildFeatures {
+        viewBinding = true
     }
 
     defaultConfig {
         applicationId = "pe.edu.cibertec.appgrupo1"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -38,11 +40,13 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    // Sobrescribimos las librerías inestables con versiones fijas y estables:
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
